7c96106beaa3c925785d0071725b4b77ca5cb007
Sam Ulrich <samson98ulrich@gmail.com>
2023-03-13 09:20:47 -0500

    Added sharding to filter generation for improved filter writes

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,8 +1,16 @@
 package cmd
 
 import (
+	"archive/tar"
 	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/klauspost/compress/gzip"
+	"github.com/sourcegraph/conc"
+	"math"
 	"os"
+	"sync/atomic"
 
 	"github.com/bits-and-blooms/bloom/v3"
 	"github.com/pterm/pterm"
@@ -14,10 +22,10 @@ func init() {
 
 	// default error rate is ~1/100M
 	genCmd.Flags().Float64P("error", "e", 0.00000001, "maximum error rate accepted by the bloom filter")
-	// default size is ~500M
-	genCmd.Flags().UintP("size", "s", 500000000, "estimated count of entities that will be inserted into the bloom filter")
 	// default max size is -1 to indicate no limit
 	genCmd.Flags().IntP("max", "m", -1, "maximum count of entities that will be inserted into the bloom filter (-1 is unlimited)")
+	// default shard count is 128
+	genCmd.Flags().IntP("shards", "s", 128, "number of shards to use for the bloom filter")
 }
 
 // genCmd
@@ -32,19 +40,6 @@ var genCmd = &cobra.Command{
 }
 
 func runGenCmd(cmd *cobra.Command, args []string) {
-	// create the bloom filter
-	es, err := cmd.Flags().GetUint("size")
-	if err != nil {
-		pterm.Fatal.Printf("failed to retrieve size flag: %v\n", err)
-		return
-	}
-	errProb, err := cmd.Flags().GetFloat64("error")
-	if err != nil {
-		pterm.Fatal.Printf("failed to retrieve error rate flag: %v\n", err)
-		return
-	}
-	filter := bloom.NewWithEstimates(es, errProb)
-
 	// open the password file
 	pass, err := os.Open(args[0])
 	if err != nil {
@@ -81,9 +76,43 @@ func runGenCmd(cmd *cobra.Command, args []string) {
 	} else {
 		info, err := os.Stat(args[0])
 		if err == nil {
-			// esitmate the size since the average line size is ~44 bytes
-			pbBuilder = pbBuilder.WithTotal(int(info.Size() / 44))
+			// estimate the size since the average line size is ~44 bytes
+			maxSize = int(info.Size() / 44)
+			pbBuilder = pbBuilder.WithTotal(maxSize)
 		}
+	}
+
+	// retrieve the error probability for the bloom filter
+	errProb, err := cmd.Flags().GetFloat64("error")
+	if err != nil {
+		pterm.Fatal.Printf("failed to retrieve error rate flag: %v\n", err)
+		return
+	}
+
+	// add a buffer size if 20% to the bloom filter max size
+	bfSize := int(float32(maxSize)*.20) + maxSize
+
+	// update filter config for the shard count
+	shardCount, err := cmd.Flags().GetInt("shards")
+	if err != nil {
+		pterm.Fatal.Printf("failed to retrieve shard count flag: %v\n", err)
+		return
+	}
+	shardSize := int(math.Ceil(float64(bfSize) / float64(shardCount)))
+	errProb *= float64(shardCount)
+
+	// print the shard info
+	pterm.Info.Printfln(
+		"\n##############################\nFilter Configuration\nShard Count: %v\nShard Size: %v\nError Probability: %v",
+		shardCount, shardSize, errProb,
+	)
+
+	// create slice to hold the shards of the filter
+	filters := make([]*bloom.BloomFilter, shardCount)
+	queues := make([]chan string, shardCount)
+	for i := range filters {
+		filters[i] = bloom.NewWithEstimates(uint(shardSize), errProb)
+		queues[i] = make(chan string)
 	}
 
 	// start the progress bar
@@ -93,31 +122,66 @@ func runGenCmd(cmd *cobra.Command, args []string) {
 		return
 	}
 
+	// create a wait group for  the shard workers
+	wg := conc.NewWaitGroup()
+
+	// create a context for the workers
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// launch the filter workers
+	for i := range filters {
+		q := queues[i]
+		wg.Go(func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case b := <-q:
+					// write to the filter
+					filters[i].AddString(b)
+					// increment the progress bar
+					pb.Increment()
+				}
+			}
+		})
+	}
+
 	// create a new scanner to preprocess each line from the
-	// password archive - this is used to trim the prevelance
+	// password archive - this is used to trim the prevalence
 	// count from each line in the archive
 	scanner := bufio.NewScanner(pass)
 
-	// create integer to track the count of entities written
-	count := 0
+	// create index to track which queue we're writing to
+	qIdx := 0
+
+	// create atomic integer to track the count of entities written
+	count := &atomic.Int64{}
 
 	// iterate the lines in the password archive adding them to the filter
 	for scanner.Scan() {
 		// skip corrupted data
 		if len(scanner.Text()) < 40 {
+			pterm.Warning.Printfln("skipping corrupted entry: %v", scanner.Text())
 			continue
 		}
 
 		// first 40 chars is sha1 sum
-		filter.AddString(scanner.Text()[:40])
-		// increment the progress bar
-		pb.Increment()
+		queues[qIdx] <- scanner.Text()[:40]
+
 		// increment the counter
-		count++
+		count.Add(1)
 
 		// exit if the count is equal to the max entities
-		if maxSize > 0 && maxSize == count {
+		if maxSize > 0 && int64(maxSize) == count.Load() {
+			cancel()
 			break
+		}
+
+		// increment the queue index
+		qIdx = qIdx + 1
+		if qIdx == len(queues) {
+			qIdx = 0
 		}
 	}
 
@@ -128,17 +192,101 @@ func runGenCmd(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	// save the filter to the output file
-	_, err = filter.WriteTo(fl)
+	// wait for the queues to clear then cancel the context
+	for {
+		cleared := true
+		for i := range queues {
+			if len(queues[i]) > 0 {
+				cleared = false
+				break
+			}
+		}
+		if cleared {
+			cancel()
+			break
+		}
+	}
+
+	// wait for the workers to finish
+	wg.Wait()
+
+	// create a tar archive with high compression
+
+	// create output file
+	outFile, err := os.Create(args[1])
 	if err != nil {
-		_, _ = pb.Stop()
-		pterm.Fatal.Printf("failed to write bloom filter: %v\n", err)
+		pterm.Fatal.Printf("failed to create output file: %v\n", err)
 		return
 	}
+	defer outFile.Close()
+
+	// create gzip writer
+	gzipWriter := gzip.NewWriter(outFile)
+	defer gzipWriter.Close()
+
+	// create tar writer
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close()
+
+	// write header for the index file
+	headerBuf := []byte(fmt.Sprintf("%d", shardCount))
+	err = tarWriter.WriteHeader(&tar.Header{
+		Name: "index",
+		Size: int64(len(headerBuf)),
+		Mode: 0666,
+	})
+	if err != nil {
+		pterm.Fatal.Printf("failed to write index header: %v\n", err)
+		return
+	}
+
+	// write index file
+	_, err = tarWriter.Write(headerBuf)
+	if err != nil {
+		pterm.Fatal.Printf("failed to write index header: %v\n", err)
+		return
+	}
+
+	// iterate through the shards
+	for i := 0; i < shardCount; i++ {
+		// write the shard to a buffer so that we know the file size
+		buf := new(bytes.Buffer)
+		_, err = filters[i].WriteTo(buf)
+		if err != nil {
+			pterm.Fatal.Printf("failed to write filter: %v\n", err)
+			return
+		}
+
+		// extract bytes from buffer
+		bufBytes := buf.Bytes()
+
+		// write header for the file
+		err = tarWriter.WriteHeader(&tar.Header{
+			Name: fmt.Sprintf("%d", i),
+			Size: int64(len(bufBytes)),
+			Mode: 0666,
+		})
+		if err != nil {
+			pterm.Fatal.Printf("failed to write header: %v\n", err)
+			return
+		}
+
+		// write buffer
+		_, err = tarWriter.Write(bufBytes)
+		if err != nil {
+			pterm.Fatal.Printf("failed to write buffer: %v\n", err)
+			return
+		}
+	}
+
+	// close the tar archive
+	_ = tarWriter.Close()
+	_ = gzipWriter.Close()
+	_ = outFile.Close()
 
 	// stop the progress bar
 	_, _ = pb.Stop()
 
 	// print the response
-	pterm.Info.Printfln("\n##############################\nDownload Complete\nOutput File: %s", args[0])
+	pterm.Info.Printfln("\n##############################\nDownload Complete\nOutput File: %s", args[1])
 }
